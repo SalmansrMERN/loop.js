211c7cf0f8fb60968d99002d841ab8dfddd38fed
Salman S. Rahman <salmansr1995@gmail.com>
2021-11-17 19:59:34 +0600

    first commit

---
+++ loop.js
@@ -0,0 +1,36 @@
+// ============ how to print from 1 to 10 ===========
+for (var i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// ============ how to print from 10 to 1 ===========
+
+for (var i = 10; i >= 1; i--) {
+    console.log(i)
+
+}
+
+
+// ============ how to print "Taskin Asghar" 10 times ===========
+
+
+for (var i = 1; i <= 10; i++) {
+    console.log("Taskin Asghar")
+}
+
+// ============ how to print even numbers from 10 to 1 ===========
+
+for (var i = 2; i <= 100; i = i + 2) {
+    console.log(i)
+}
+
+// ============ sum of 1 to 5 ===========
+
+var sum = 0;
+
+for (var i = 1; i <= 5; i++) {
+    sum = sum + i;
+
+}
+
+console.log("Total = " + sum)
